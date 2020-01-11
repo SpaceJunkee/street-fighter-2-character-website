e5fa0ae5f647e7d1bddf4b5532bd35882cb03e78
Kevin <ksandy@outlook.ie>
2020-01-11 21:40:41 +0000

    Change bio layout for ryu, change homepage button on bio page, add more theme songs and included javascript audio player when icon clicked

--- javascript/audioplayer.js
+++ javascript/audioplayer.js
@@ -1,8 +1,34 @@
 /* eslint-env browser */
 
+//Find the theme songs in mp3 folder
 
-var ryuTheme = new Audio('../mp3/09.%20Ryu%20Stage.mp3');
+var stageSongs = new Array(10);
 
-        function playThemeSong(ryu) {
-            ryuTheme.play();
-        }
+stageSongs[0] = new Audio('../mp3/09.%20Ryu%20Stage.mp3');
+stageSongs[1] = new Audio('../mp3/05.%20Ken%20Stage.mp3');
+stageSongs[2] = new Audio('../mp3/30.%20E.Honda%20Stage.mp3');
+stageSongs[3] = new Audio('../mp3/05.%20Ken%20Stage.mp3');
+stageSongs[4] = new Audio('../mp3/05.%20Ken%20Stage.mp3');
+stageSongs[5] = new Audio('../mp3/05.%20Ken%20Stage.mp3');
+
+
+//Play theme songs corresponding to characters
+if(document.getElementById("ryuTheme")){
+    document.getElementById("ryuTheme")
+        .addEventListener('click', function (event) {
+            stageSongs[0].play();
+        });
+}else if(document.getElementById("kenTheme")){
+     document.getElementById("kenTheme")
+        .addEventListener('click', function (event) {
+            stageSongs[1].play();
+        });
+}
+
+
+
+
+    
+
+
+
